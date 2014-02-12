fc3dc7e2a8acd79ab11b17a6d5e142c061930808
Alexander Vasilyev <a.vasilyev@linus.su>
2014-02-12 14:19:30 +0400

    settings upd

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
-from .base import *
+from .common import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+"""
 INSTALLED_APPS += (
     'debug_toolbar',
 )
@@ -17,3 +18,4 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+"""
