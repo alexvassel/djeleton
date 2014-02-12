4815b5ede4d669fe2e9035134c4a8f6d0c5164f3
Alexander Vasilyev <a.vasilyev@linus.su>
2014-02-12 15:49:29 +0400

    simple fabric

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+from fabric.api import *
+
+dev = '192.168.1.55'
+
+env.user = 'linus'
+env.hosts = (dev,)
+env['password'] = 'linus_2013'
+
+PROJECT_NAME = '{{project_name}}'
+
+
+@hosts(dev)
+def update_remote(branch='master', restart=False):
+    """
+    Checks out given branch, pulls then pushs on localhost
+    and pulls on dev + reloads uwsgi
+
+    :branch: branch to checkout on localhost.
+    :restart:  restart uwsgi?
+    """
+    puts('Checkout branch %s on localhost' % branch)
+    local('git checkout %s' % branch)
+    puts('Pull on localhost')
+    local('git pull origin %s' % branch)
+    puts('Push from localhost')
+    local('git push origin %s' % branch)
+    puts('Pull on %s' % dev)
+    env['cwd'] = '/home/' + env.user + '/projects/' + PROJECT_NAME + '/source/'
+    puts('Checkout branch %s on %s' % (branch, dev))
+    run('git checkout %s' % branch)
+    run('git pull origin %s' % branch)
+    puts('Restart uwsgi on %s' % (dev))
+    if restart:
+        run('touch /home/linus/projects/' + PROJECT_NAME + '/')
