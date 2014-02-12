817be35bf0e727e4c709cc12f0a619364f1ae036
Alexander Vasilyev <a.vasilyev@linus.su>
2014-02-12 14:54:04 +0400

    with this errors with no database

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -26,8 +26,6 @@ DATABASES = {
 }
 
 
-SESSION_SAVE_EVERY_REQUEST = True
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
