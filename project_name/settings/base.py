7d993baf239faeb3d5d5293d5ff30a4363c69f42
Alexander Vasilyev <a.vasilyev@linus.su>
2014-02-12 11:22:23 +0400

    djeleton -> {{project_name}} for tpl engine

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -1,4 +1,4 @@
-# Django settings for djeleton project.
+# Django settings for {{project_name}} project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -101,10 +101,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'djeleton.urls'
+ROOT_URLCONF = '{{project_name}}.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'djeleton.wsgi.application'
+WSGI_APPLICATION = '{{project_name}}.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
